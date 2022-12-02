77a93fe5daba72c11a2fc0bfa79cc799a14b9cf6
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2022-12-02 18:55:29 +0530

    Program to Find PerfectNumber in given Range

---
+++ LogicalPrograms/LogicalPrograms/PerfectNumber.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public static class PerfectNumber
+    {
+        public static void perfectNumber()
+        {
+            int n, i, sum;
+            int mn, mx;
+
+            Console.WriteLine("Find the perfect number within a given range\n");
+
+            Console.WriteLine("Enter the starting range or number = ");
+            mn = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Enter the ending range or number = ");
+            mx = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(".......................................");
+            for (n = mn; n <= mx; n++)
+            {
+                i = 1;
+                sum = 0;
+                while (i < n)
+                {
+                    if (n % i == 0)
+                    {
+                        sum = sum + i;
+                    }
+                    i++;
+                }
+                if (sum == n)
+                {
+                    Console.WriteLine("{0}", n);
+                }
+
+
+            }
+        }
+    }
+}
--- LogicalPrograms/LogicalPrograms/Program.cs
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -5,7 +5,7 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to LogicalProgram");
-            FibonacciSeries.fibonacciSries();
+            PerfectNumber.perfectNumber();
         }
     }
 }
