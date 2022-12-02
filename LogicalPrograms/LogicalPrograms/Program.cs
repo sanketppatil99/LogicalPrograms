6cdf4144909b318fc5de2569c5e7f2ec6c11133c
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2022-12-02 19:24:20 +0530

    Program for Revers number

--- LogicalPrograms/LogicalPrograms/Program.cs
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -5,7 +5,7 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to LogicalProgram");
-            PrimeNumber.primeNumber();
+            ReverseNumber.reverseNumber();
         }
     }
 }
---
+++ LogicalPrograms/LogicalPrograms/ReverseNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public static class ReverseNumber
+    {
+        public static void reverseNumber()
+        {
+            int n, reverse = 0, rem;
+            Console.WriteLine("Enter the Number = ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            while (n != 0)
+            {
+                rem = n % 10;
+                reverse = reverse * 10 + rem;
+                n = n / 10;
+            }
+            Console.WriteLine("Reverse Number is = " + reverse);
+        }
+    }
+}
