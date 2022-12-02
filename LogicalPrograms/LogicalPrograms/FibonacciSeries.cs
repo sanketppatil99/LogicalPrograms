a3b47413b61081b1b854e4effe69052c2e127eda
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2022-12-02 18:38:57 +0530

    Program to fibonacci Series upto given Number

---
+++ LogicalPrograms/LogicalPrograms/FibonacciSeries.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public static class FibonacciSeries
+    {
+        public static void fibonacciSries()
+        {
+            int n1 = 0, n2 = 1, n3, i, number;
+
+            Console.WriteLine("Enter the number of Elements = ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(n1 + " " + n2 + " ");
+
+            for (i = 2; i < number; i++)
+            {
+                n3 = n1 + n2;
+                Console.WriteLine(n3 + " ");
+
+                n1 = n2;
+                n2 = n3;
+
+            }
+        }
+    }
+}
---
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LogicalPrograms
+{
+    class Programs
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to LogicalProgram");
+            FibonacciSeries.fibonacciSries();
+        }
+    }
+}
