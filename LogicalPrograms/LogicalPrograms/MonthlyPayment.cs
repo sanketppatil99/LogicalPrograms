d84b6a938ea70050e6cae1d398d1c5648823f6bf
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2022-12-03 21:46:59 +0530

    Program to Calulate Monthly payment

---
+++ LogicalPrograms/LogicalPrograms/MonthlyPayment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class MonthlyPayment
+    {
+        public static void monthlyPayment()
+        {
+            Console.WriteLine("Enter principal loan amount P = ");
+            int P = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("R percent interest compounded monthly = ");
+            int R = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Year you want Monthly Payment Y = ");
+            int Y = Convert.ToInt32(Console.ReadLine());
+
+            int n = 12 * Y;
+            int r = R / (12 * 100);
+            int Payment = ( P * r )/ 1 -(1 + r)^(-n);
+            Console.WriteLine("Monthly Payment is = {0}", Payment);
+        }
+    }
+}
--- LogicalPrograms/LogicalPrograms/Program.cs
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -5,7 +5,7 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to LogicalProgram");
-            TemperaturConversion.temperaturConversion();
+            MonthlyPayment.monthlyPayment();
         }
     }
 }
