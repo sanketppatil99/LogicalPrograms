fd635cf1a6d5d4202b9a8add9639c99ae2ecf65a
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2022-12-03 21:04:34 +0530

    Find DayofWeek using Gregorian calendar

---
+++ LogicalPrograms/LogicalPrograms/DayOfWeek.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class DayOfWeek
+    {
+        public static void dayOfWeek()
+        {
+            Console.WriteLine("Enter the Day = ");
+            int day = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Month = ");
+            int month = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Year = ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("{0} / {1} / {2}",day,month,year);
+
+            int y0 = year - (14 - month)/12;
+            int x = y0 + y0 / 4 - y0 / 100 + y0 / 400;
+            int m0 = month + 12 * ((14 - month) / 12) - 2;
+            int d0 = (day + x + (31 * m0) / 12) % 7;
+
+            switch(d0)
+            {
+                case 0:
+                    Console.WriteLine("Sunday");
+                    break;
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+
+            }
+
+        }
+    }
+}
--- LogicalPrograms/LogicalPrograms/Program.cs
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -5,7 +5,7 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to LogicalProgram");
-            SimulateStopwatch.simulateStopwatch();
+            DayOfWeek.dayOfWeek();
         }
     }
 }
