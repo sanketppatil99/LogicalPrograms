bb57a743cc2258482754ede34ca60158270f4178
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2022-12-03 15:52:11 +0530

    Program For StopWatch

--- LogicalPrograms/LogicalPrograms/Program.cs
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -5,7 +5,7 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to LogicalProgram");
-            CouponNo.CheckCouponNumber();
+            SimulateStopwatch.simulateStopwatch();
         }
     }
 }
---
+++ LogicalPrograms/LogicalPrograms/SimulateStopwatch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public static class SimulateStopwatch
+    {
+        public static void simulateStopwatch()
+        {
+            
+            Stopwatch stopwatch = new Stopwatch();
+            Console.WriteLine("Stopwatch is Started");
+            stopwatch.Start();
+
+            Console.WriteLine("To stop stopwatch Enter 1 = ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            
+            if (a == 1)
+            {
+                stopwatch.Stop();
+            }
+            Console.WriteLine("Time Elapsed : {0}",stopwatch.Elapsed);
+            Console.ReadLine();
+
+        }
+    }
+}
